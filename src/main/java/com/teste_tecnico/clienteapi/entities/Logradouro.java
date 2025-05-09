4479613e6d75df95dbd0ff4fa633374d6e0dab85
Denilson <de_nilson_filho@hotmail.com>
2025-05-09 12:41:29 -0300

    feat: adicionei alguns campos no Logradouro para poder fazer buscas com procedures, pois nao eh recomendo fazer as consultas basicas(CRUD) que o JPA faz

--- src/main/java/com/teste_tecnico/clienteapi/entities/Logradouro.java
+++ src/main/java/com/teste_tecnico/clienteapi/entities/Logradouro.java
@@ -19,10 +19,53 @@ public class Logradouro {
     private Long id;
 
     @NotBlank
-    private String descricao;
+    private String endereco;
+    @NotBlank
+    private String cidade;
+    @NotBlank
+    private String uf;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
+    public Logradouro(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
